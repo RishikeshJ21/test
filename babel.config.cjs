0f5a7f580695fffe6b718c79186fe3b21669f140
Jitendra39 <jeetendrachoudhari213@gmail.com>
2025-04-29 18:42:44 +0530

    fix

--- babel.config.cjs
+++ babel.config.cjs
@@ -5,31 +5,48 @@ module.exports = {
       {
         targets: {
           node: "current",
-          browsers: [">0.25%", "not ie 11", "not op_mini all"]
+          browsers: [
+            ">0.2%",
+            "not dead",
+            "ie >= 11",
+            "safari >= 9",
+            "firefox >= 52",
+            "chrome >= 49",
+            "edge >= 16",
+            "opera >= 36",
+            "ios >= 9",
+            "android >= 5",
+          ],
         },
         useBuiltIns: "usage",
         corejs: "3.41.0",
         bugfixes: true,
         loose: false,
         modules: false,
-        exclude: ["transform-typeof-symbol"]
-      }
+        exclude: ["transform-typeof-symbol"],
+      },
     ],
-    ["@babel/preset-react", {
-      runtime: "automatic"
-    }],
-    "@babel/preset-typescript"
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
+    "@babel/preset-typescript",
   ],
   plugins: [
     "babel-plugin-macros",
-    ["@babel/plugin-transform-runtime", {
-      corejs: {
-        version: 3,
-        proposals: true
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        corejs: {
+          version: 3,
+          proposals: true,
+        },
+        helpers: true,
+        regenerator: true,
+        useESModules: true,
       },
-      helpers: true,
-      regenerator: true,
-      useESModules: true
-    }]
-  ]
-} 
+    ],
+  ],
+};
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,41 @@
 export default {
   plugins: {
-    'tailwindcss/nesting': {},
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
     tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer: {
+      overrideBrowserslist: [
+        ">0.2%",
+        "not dead",
+        "ie >= 11",
+        "safari >= 9",
+        "firefox >= 52",
+        "chrome >= 49",
+        "edge >= 16",
+        "opera >= 36",
+        "ios >= 9",
+        "android >= 5",
+      ],
+      grid: true,
+    },
+    "postcss-preset-env": {
+      features: {
+        "nesting-rules": false,
+      },
+      stage: 3,
+      browsers: [
+        ">0.2%",
+        "not dead",
+        "ie >= 11",
+        "safari >= 9",
+        "firefox >= 52",
+        "chrome >= 49",
+        "edge >= 16",
+        "opera >= 36",
+        "ios >= 9",
+        "android >= 5",
+      ],
+    },
+    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
   },
-}
+};
